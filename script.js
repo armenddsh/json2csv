795aec51bedf061a164266c8d44be0b12307a1ca
Armend Shala <armend.shala@telekom.de>
2024-06-29 00:06:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const data = require("./example.json")
+// const fs = require("fs")
+
+function flatten(rowIndex, obj, parentKey, out, headers) {
+    const keys = Object.keys(obj);
+
+    for (const key of keys) {
+        const value = obj[key]
+        const k = parentKey ? parentKey + "." + key : key;
+        if (Array.isArray(value)) {
+            for (const v of value) {
+                flatten(rowIndex, v, k, out, headers)
+            }
+        } else if (value instanceof Object) {
+            flatten(rowIndex, value, k, out, headers)
+        } else {
+            headers.add(k);
+            out.push({
+                rowIndex,
+                name: k,
+                value: value
+            })
+        }
+    }
+}
+
+function json2csv(obj) {
+    const out = [];
+    const headers = new Set();
+
+    flatten(0, obj, "", out, headers)
+
+    const p = {}
+    const results = []
+    const headerProps = {}
+    for (const header of headers) {
+        const group = out.filter(f => f.name === header)
+        if (group.length > 0) {
+            p[header] = group
+        }
+        headerProps[header] = null;
+    }
+
+    for (const pp in p) {
+        const r = p[pp];
+        if (r.length > 1) {
+            for (const m of r) {
+                headerProps[pp] = m['value']
+                results.push({
+                    ...headerProps
+                })
+            }
+        } else if (!Array.isArray(r[0])) {
+            headerProps[pp] = r[0]['value']
+        }
+
+    }
+
+    
+    return results
+}
+
+const results = json2csv(data);
+
+// fs.writeFileSync("out.json", JSON.stringify(results))
